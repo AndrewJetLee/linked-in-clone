8b4bce1e19fd90f0293c73eb696addc241e96fac
AndrewJetLee <andrewjetlee777@gmail.com>
2021-12-02 21:30:10 -0600

    Added loading svg when post is uploading to database

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,21 +1,29 @@
-import styled from 'styled-components';
-import Post from './Post';
-import ArticleCard from './ArticleCard';
+import styled from "styled-components";
+import Post from "./Post";
+import ArticleCard from "./ArticleCard";
+import { useSelector } from "react-redux";
 
-
-
-export const Main = ({user, dispatch}) => {
-    return (
-        <Container>
-            <Post user={user} dispatch={dispatch}/>
-            <ArticleCard />
-        </Container>
-        
-    )
-}
+export const Main = ({ user, dispatch }) => {
+  const loading = useSelector((state) => state.article.loading);
+  return (
+    <Container>
+      <Post user={user} dispatch={dispatch} />
+      {loading ? (
+        <Loading><img src="/images/loading.svg" alt="" /></Loading>
+      ) : null}
+      <ArticleCard />
+    </Container>
+  );
+};
 
 const Container = styled.div`
-    grid-area: Main; 
-`
+  grid-area: Main;
+`;
 
-
+const Loading = styled.div`
+  text-align: center;
+  img {
+    width: 30px;
+    text-align: center;
+  }
+`;
--- src/redux/articleSlice.js
+++ src/redux/articleSlice.js
@@ -7,6 +7,7 @@ import { collection, doc, setDoc, addDoc } from "firebase/firestore";
 export const postArticleAPI = (article) => {
   return (dispatch, getState) => {
     // if image exists, store the image or video into cloud
+    dispatch(setLoadingStatus(true));
     if (article.image) {
       const storageRef = ref(storage, `/images/${article.image.name}`);
       const uploadTask = uploadBytesResumable(storageRef, article.image);
@@ -38,6 +39,7 @@ export const postArticleAPI = (article) => {
               .then((payload) => {
                 console.log("Added article: ", payload);
                 dispatch(updateArticles(article));
+                dispatch(setLoadingStatus(false));
               })
               .catch((err) => {
                 console.log(err);
@@ -46,6 +48,7 @@ export const postArticleAPI = (article) => {
         }
       );
     } else {
+      dispatch(setLoadingStatus(true));
       const articlesRef = collection(db, "articles");
       addDoc(articlesRef, {
         actor: {
@@ -61,6 +64,7 @@ export const postArticleAPI = (article) => {
         .then((payload) => {
           console.log("Added article: ", payload);
           dispatch(updateArticles(article));
+          dispatch(setLoadingStatus(false));
         })
         .catch((err) => {
           console.log(err);
@@ -77,16 +81,20 @@ export const postArticleAPI = (article) => {
 export const articleSlice = createSlice({
   name: "article",
   initialState: {
+    loading: false,
     articles: [],
   },
   reducers: {
     updateArticles(state, action) {
-        state.articles.push(action.payload)
+      state.articles.push(action.payload)
     },
+    setLoadingStatus(state, action) {
+      state.loading = action.payload;
+    }
   },
 });
 
 // Action creators are generated for each case reducer function
-export const { updateArticles } = articleSlice.actions;
+export const { updateArticles, setLoadingStatus } = articleSlice.actions;
 
 export default articleSlice.reducer;
