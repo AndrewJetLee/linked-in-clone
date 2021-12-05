fd2b6b46ab0e24226727dbc7e50b91db391b1cf4
AndrewJetLee <andrewjetlee777@gmail.com>
2021-12-05 16:44:49 -0600

    Polished Landing Component

--- src/App.js
+++ src/App.js
@@ -3,33 +3,31 @@ import { ThemeProvider } from "styled-components";
 import "./App.css";
 import Landing from "./components/Landing";
 import Home from "./components/Home";
-import Login from './components/Login'
-import Signup from './components/Signup'
+import Login from "./components/Login";
+import Signup from "./components/Signup";
 
 let theme = {
   mobile: "768px",
   colors: {
     greyPrimaryBg: "rgb(243, 242, 239)",
     bluePrimary: "rgb(10, 102, 194)",
-  }
+  },
 };
 
 function App() {
   return (
-    
     <ThemeProvider theme={theme}>
       <div className="App">
         <Router>
           <Routes>
-            <Route exact path="/signup" element={<Signup />}></Route>
-            <Route exact path="/login" element={<Login />}></Route>
-            <Route exact path="/home" element={<Home />}></Route>
-            <Route exact path="/" element={<Landing />} />
+            <Route path="/home" element={<Home />}></Route>
+            <Route path="/signup" element={<Signup />}></Route>
+            <Route path="/login" element={<Login />}></Route>
+            <Route path="/" element={<Landing />} />
           </Routes>
         </Router>
       </div>
     </ThemeProvider>
-    
   );
 }
 
